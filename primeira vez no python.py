7539c252a3cd76ba732789df38b7a2144ca1cd76
boredflush <tavinholuizmarqx@gmail.com>
2026-03-20 14:47:29 -0300

    Add files via upload

---
+++ primeira vez no python.py
@@ -0,0 +1,10 @@
+A= int(input("digite o valor de A:"))
+
+B= int(input("digite o valor de B:"))
+
+if A > B:
+    print ("A é maior que B")
+if A < B:
+    print ("B é maior que A")
+if A == B: 
+    print("A e B sao iguais")
